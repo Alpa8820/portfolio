65ec4dd4ccc9db3a857023ec6aa354dde98c55d2
alpa882 <apla8820@gmail.com>
2025-07-29 18:52:51 +0900

    Fix : Update files.

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -41,22 +41,22 @@ const Navbar = () => {
       <div className="navbarRight">
         <div className="icons new_font">
           <a href="#" rel="noreferrer">
-            Home&nbsp;
+            <b>H</b>ome&nbsp;
           </a>
           <a href="#more" rel="noreferrer">
-            Skill&nbsp;
+            <b>S</b>kill&nbsp;
           </a>
           <a href="#hachathons" rel="noreferrer">
-            Hackathon&nbsp;
+            <b>H</b>ackathon&nbsp;
           </a>
           <a href="#project" rel="noreferrer">
-            Project&nbsp;
+            <b>P</b>roject&nbsp;
           </a>
           <a href="#terminal" rel="noreferrer">
-            Terminal&nbsp;
+            <b>T</b>erminal&nbsp;
           </a>
           <a href="#contact" rel="noreferrer">
-            Contact
+            <b>C</b>ontact
           </a>
         </div>
         {/* {language === "en" ? (
